efc84d48e9da397a8ffe29a15dbe74f74ea9e9ed
twede10 <twede10@gmail.com>
2023-01-07 15:00:46 -0700

    Prep2 Base Assignment Completed. Not Stretch Challenge.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade%: ");
+        string gradeString = Console.ReadLine();
+
+        int gradePercent = int.Parse(gradeString);
+        string gradeLetter;
+        if (gradePercent == 90 || gradePercent > 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercent == 80 || gradePercent > 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercent == 70 || gradePercent > 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercent == 60 || gradePercent > 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        string note;
+        if (gradePercent == 70 || gradePercent >70)
+        {
+            note = "Congradulations on passing the class!";
+        }
+        else
+        {
+            note = "Good luck next time around.";
+        }
+
+        Console.WriteLine($"Your grade: {gradeLetter}, Note: {note}");
     }
 }
