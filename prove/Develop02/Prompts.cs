221c7be9f7673feee06121763aaaa3722723aa91
twede10 <twede10@gmail.com>
2023-01-28 19:37:08 -0700

    added more prompts

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -16,6 +16,16 @@ public class Prompts
             ,"Write about a challenge you faced and how you overcame it"
             ,"Reflect on a mistake you made and what you learned from it"
             ,"Describe a person, place, or thing that makes you feel nostalgic."
+            ,"List three things you're grateful for today"
+            ,"Describe a difficult conversation you had and how you handled it"
+            ,"Write about a time when you felt proud of yourself"
+            ,"Reflect on a time when you learned something new about yourself"
+            ,"Describe a person who inspires you and why"
+            ,"Write about a time when you felt truly connected to others"
+            ,"Reflect on a time when you felt disconnected or alone"
+            ,"Describe a moment of beauty you witnessed today"
+            ,"Write about a goal you're working towards and how you plan to achieve it"
+            ,"Reflect on a time when you showed compassion or empathy towards someone."
         };
         int index = randomPrompt.Next(list.Count);
         return list[index];
